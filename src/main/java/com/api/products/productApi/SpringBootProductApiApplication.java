deee9a2a1eae8633fdbf1b49ab17a55aab3016ea
Sheikh Ahmed <sheikhishtiak@gmail.com>
2024-11-18 01:01:00 -0500

    Initial commit

---
+++ src/main/java/com/api/products/productApi/SpringBootProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.products.productApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/products/productApi/SpringBootProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.products.productApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
