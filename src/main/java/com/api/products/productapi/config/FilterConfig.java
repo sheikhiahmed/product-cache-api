a827eff4e3dd58079452af02afe75133d2a47e96
Sheikh Ahmed <sheikhishtiak@gmail.com>
2024-11-20 23:01:20 -0500

    added loging information features

---
+++ src/main/java/com/api/products/productapi/config/FilterConfig.java
@@ -0,0 +1,18 @@
+package com.api.products.productapi.config;
+
+import com.api.products.productapi.filter.LoggingFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> loggingFilterFilterRegistrationBean(){
+        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoggingFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        return registrationBean;
+    }
+}
---
+++ src/main/java/com/api/products/productapi/filter/LoggingFilter.java
@@ -0,0 +1,34 @@
+package com.api.products.productapi.filter;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@WebFilter("/*")
+public class LoggingFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException{
+        log.info("Initializing filter: LoggingFilter");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String url = httpServletRequest.getRequestURL().toString();
+        String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        System.out.println("url: " + url + ", date and time: " + timeStamp);
+
+        chain.doFilter(request, response);
+    }
+
+
+    @Override
+    public void destroy(){
+        log.info("destroy filter: LoggingFilter");
+    }
+}
