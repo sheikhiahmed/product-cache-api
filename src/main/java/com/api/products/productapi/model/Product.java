50689b6922415531244d6784dfd64c5d305572d4
Md Ariful Haq <akmarif@yahoo.com>
2024-11-21 19:00:28 +0100

    missing auto generated id annotation and lombok annotation for getter.

--- src/main/java/com/api/products/productapi/model/Product.java
+++ src/main/java/com/api/products/productapi/model/Product.java
@@ -2,19 +2,23 @@ package com.api.products.productapi.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Entity
+@Data
 public class Product {
 
-   @Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotNull(message ="Name is mandatory")
+    @NotNull(message = "Name is mandatory")
     @Size(min = 2, max = 100, message = "Product name must be between 2 and 100 characters")
     private String name;
 
