1d1fe0b0df7b05f3f6353500101c6c1b39a91b48
Sheikh Ahmed <sheikhishtiak@gmail.com>
2024-11-21 00:05:01 -0500

    input validation applied

--- src/main/java/com/api/products/productapi/model/Product.java
+++ src/main/java/com/api/products/productapi/model/Product.java
@@ -3,7 +3,9 @@ package com.api.products.productapi.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Product {
@@ -12,11 +14,13 @@ public class Product {
     private Long id;
 
     @NotBlank(message ="Name is mandatory")
+    @Size(min = 2, max = 100, message = "Product name must be between 2 and 100 characters")
     private String name;
 
     private String description;
 
     @NotBlank(message = "Price is mandatory")
+    @Min(value = 0, message = "Price must be a positive value")
     private Double price;
 
     @NotBlank(message = "Category is mandatory")
